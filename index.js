19785b1d5ed0d4eeb6e49b7d1f9479318161a266
ABVenu <avenugopalb@gmail.com>
2025-02-17 12:05:34 +0530

    Test Feature

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config()
+const PORT = process.env.PORT || 8080
+const app = express();
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.status(200).json({msg:"This is test route"})
+})
+app.listen(PORT, ()=>{
+    console.log("Server started on Port", PORT)
+})
