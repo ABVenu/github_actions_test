6fcded681026977ed565343b419f0847984d9e9a
ABVenu <avenugopalb@gmail.com>
2025-02-17 13:23:12 +0530

    Keep calm and commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-require("dotenv").config()
-const PORT = process.env.PORT || 8080
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.status(200).json({msg:"This is test route"})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "This is test route" });
+});
 
-app.get("/home", (req,res)=>{
-    res.status(200).json({msg:"This is home route"})
-})
-app.listen(PORT, ()=>{
-    console.log("Server started on Port", PORT)
-})
+app.get("/home", (req, res) => {
+  res.status(200).json({ msg: "This is home route" });
+});
+app.listen(PORT, () => {
+  console.log("Server started on Port", PORT);
+});
